ead884e705e60e5436c8eddb75ac452afcf89fed
abolajii <abolajiadeajayi@gmail.com>
2024-08-08 08:36:17 +0100

    first commit

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,6 +1,5 @@
-import Checkbox from "../components/Checkbox"; // Adjust the import path as necessary
-import { MdFilterList } from "react-icons/md";
-import React from "react";
+import { MdWorkHistory } from "react-icons/md";
+// import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -31,48 +30,68 @@ const Line = styled.div`
   margin-top: 5px;
 `;
 
-const Filter = styled.div`
+// const Filter = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   height: 30px;
+//   width: 30px;
+//   background: linear-gradient(to left, #2d2d2d, #7e967f);
+//   cursor: pointer;
+//   margin-right: 10px;
+
+//   &:hover {
+//     background-color: rgba(192, 227, 192, 0.1);
+//   }
+
+//   &:active {
+//     transform: scale(1.04);
+//   }
+// `;
+
+// const FilterContent = styled.div<{ isVisible: boolean }>`
+//   max-height: ${(props) => (props.isVisible ? "400px" : "0")};
+//   height: 20px;
+//   overflow: hidden;
+//   transition: max-height 0.3s ease-in-out;
+// `;
+
+const IconContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  /* border: 1px solid rgba(0, 255, 0, 0.07); */
   height: 30px;
   width: 30px;
   background: linear-gradient(to left, #2d2d2d, #7e967f);
-  cursor: pointer;
-  margin-right: 10px;
-
-  &:hover {
-    background-color: rgba(192, 227, 192, 0.1);
-  }
-
-  &:active {
-    transform: scale(1.04);
-  }
-`;
-
-const FilterContent = styled.div<{ isVisible: boolean }>`
-  max-height: ${(props) => (props.isVisible ? "400px" : "0")};
-  height: 20px;
-  overflow: hidden;
-  transition: max-height 0.3s ease-in-out;
+  /* border-radius: 50%; */
 `;
 
 const Work = () => {
-  const [isFilterContentVisible, setIsFilterContentVisible] =
-    React.useState(false);
+  //   const [isFilterContentVisible, setIsFilterContentVisible] =
+  //     React.useState(false);
 
-  const toggleFilterContent = () => {
-    setIsFilterContentVisible((prev) => !prev);
-  };
+  //   const toggleFilterContent = () => {
+  //     setIsFilterContentVisible((prev) => !prev);
+  //   };
 
   return (
     <Container>
       <div className="flex">
         <div className="mr-md">
-          <Heading>My Projects</Heading>
+          <Heading>my projects</Heading>
           <Line />
         </div>
-        <div className="flex">
+        <IconContainer className="flex center">
+          <MdWorkHistory size={20} color="#ffffff" />
+        </IconContainer>
+      </div>
+      {/* <div className="flex">
+        <div className="mr-md flex-1">
+          <Heading>Projects</Heading>
+          <Line />
+        </div>
+        {/* <div className="flex">
           <Filter onClick={toggleFilterContent}>
             <MdFilterList color="#fff" size={20} />
           </Filter>
@@ -84,20 +103,16 @@ const Work = () => {
             <Checkbox color="#ff9800" label="Done" />
             <Checkbox color="#4caf50" label="On going" />
           </FilterContent>
-        </div>
-      </div>
+        </div> 
+      </div> 
+      */}
       <div>
         <div className="flex">
           <p className="project">
-            Welcome! Below are the projects I have worked on. Please walk with
-            me as I showcase my experience and the skills I have honed over the
-            years. From frontend development using React to backend scripting
-            with Node.js, I've delved into various technologies to create
-            seamless and efficient solutions. Whether it's building robust
-            databases with MongoDB and MySQL or implementing scalable state
-            management with Redux, my journey has been rich with learning and
-            growth. Let's explore these projects together, where each line of
-            code tells a story of passion, commitment, and innovation.
+            Welcome! Below are the projects where I got my hands dirty. These
+            projects showcase my expertise in full-stack development, from
+            frontend interfaces to backend databases, highlighting my diverse
+            skill set and commitment to quality.
           </p>
         </div>
       </div>
